cc9efc91df60f052fc68c1ab1eeb71bb1d63b567
Thomas Pinkava <pinkavaprime@gmail.com>
2020-02-16 22:50:50 -0600

    added overlays and slight dynamism to mockup

--- app/controllers/xmucane_controller.rb
+++ app/controllers/xmucane_controller.rb
@@ -10,15 +10,13 @@ class XmucaneController < ApplicationController
         @test_date.gregorian = Date.today
     end
   
-    @@popolVuhQuotes = ["Whatever there is that might be is simply not there.",
-                        "Only the sea alone is pooled under all the sky; there is nothing whatever gathered together.",
-                        "The Lords of Xibalba were seated there; but they were just mannequins, woodcarvings.",
-                        "Shame on you, Heart of Sky! Attempt no treachery before the countenance of Sovereign Plumed Serpent!",
-                        "For the making of Earth they simply said \"Earth\".",
-                        "Every Day, Every Morn, In the Dark, In the Dawn, R-r-r-rip, R-r-r-rip, R-r-r-rub, R-r-r-rub, Right in our faces, because of you!"]
 
     def home
-        @rand_placeholder = @@popolVuhQuotes.shuffle.first
+        # For generating the date dynamically, there are three parts.
+        # Long Count Radices are repeated five times; the numerals alone change.
+        # Two Calendar Rounds (both numerals and glyphs change)
+        # One ISIG (or Night Lord if we change that) (either one is a single change)
+        # The generative function must set the image file, the alt-text, and the overlay if we're doing that.
     end
 
     def about
--- app/helpers/xmucane_helper.rb
+++ app/helpers/xmucane_helper.rb
@@ -1,2 +1,7 @@
 module XmucaneHelper
+
+    def numeral_space(numeral)
+        [0, numeral-1].max / 5 + 1
+    end
+
 end
