a65816fa4a7da044a7b7ffd14f99e95b4fef478c
Thomas Pinkava <pinkavaprime@gmail.com>
2020-02-17 08:45:37 -0600

    mayan date is now dynamic

--- app/controllers/xmucane_controller.rb
+++ app/controllers/xmucane_controller.rb
@@ -2,21 +2,9 @@ require 'mayan_date'
 
 class XmucaneController < ApplicationController
 
-    def initialize
-        # I *think* this doesn't break anything
-        super()
-        # Prep a mayan date visible to all pages (TODO perhaps move this to just the main page controller later)
-        @test_date = MayanDate.new
-        @test_date.gregorian = Date.today
-    end
-  
-
     def home
-        # For generating the date dynamically, there are three parts.
-        # Long Count Radices are repeated five times; the numerals alone change.
-        # Two Calendar Rounds (both numerals and glyphs change)
-        # One ISIG (or Night Lord if we change that) (either one is a single change)
-        # The generative function must set the image file, the alt-text, and the overlay if we're doing that.
+        @mayan_date = MayanDate.new
+        @mayan_date.gregorian = Date.today
     end
 
     def about
--- app/helpers/xmucane_helper.rb
+++ app/helpers/xmucane_helper.rb
@@ -1,5 +1,6 @@
 module XmucaneHelper
 
+
     def numeral_space(numeral)
         [0, numeral-1].max / 5 + 1
     end
