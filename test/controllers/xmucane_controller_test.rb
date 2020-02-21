9a1c5f5c089227ca3f5c4095c53bf4ab41d3ae5f
Thomas Pinkava <pinkavaprime@gmail.com>
2020-02-21 12:39:13 -0600

    HW6

--- test/controllers/xmucane_controller_test.rb
+++ test/controllers/xmucane_controller_test.rb
@@ -1,19 +1,28 @@
 require 'test_helper'
 
 class XmucaneControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get xmucane_home_url
-    assert_response :success
-  end
 
-  test "should get about" do
-    get xmucane_about_url
-    assert_response :success
-  end
+    def setup
+        @base_title = "Xmucane"
+    end
 
-  test "should get glyphs" do
-    get xmucane_glyphs_url
-    assert_response :success
-  end
+    test "should get home" do
+        get home_url
+        assert_response :success
+        assert_select "title", "#{@base_title}", 'title incorrect'
+        refute_nil assigns(:mayan_date), 'mayan date object absent'
+    end
+
+    test "should get about" do
+        get about_url
+        assert_response :success
+        assert_select "title", "About - #{@base_title}"
+    end
+
+    test "should get glyphs" do
+        get glyphs_url
+        assert_response :success
+        assert_select "title", "Glyphs - #{@base_title}"
+    end
 
 end
