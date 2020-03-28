1d17dc19dd5bd5e3cb4fd39dd19463696c39b3aa
Thomas Pinkava <pinkavaprime@gmail.com>
2020-03-28 15:26:44 -0700

    Added date search form functionality

--- app/controllers/xmucane_controller.rb
+++ app/controllers/xmucane_controller.rb
@@ -4,14 +4,42 @@ class XmucaneController < ApplicationController
 
     def home
         @mayan_date = MayanDate.new
-        if params[:date] then
-            # Set the mayan date from the URL
-            @mayan_date.gregorian = Date.strptime(params[:date], '%Y-%m-%d') 
+        if params[:commit] == 'From Gregorian Date' then
+
+            # Set the mayan date to the requested Gregorian Date from the URL
+            year = params[:date][:year].to_i
+            year = 1 - year if params[:date][:era] == 'BCE'
+            begin
+                @mayan_date.gregorian = Date.new(year, params[:date][:month].to_i, params[:date][:day].to_i)
+            rescue
+                # In the case of an invalid date URL, reset to the dawn of creation
+                @mayan_date.mayan_day = 0;
+            end
+
+        elsif params[:commit] == 'From Long Count' then
+
+            # Set the mayan date to the requested Long Count Date from the URL
+            begin
+                @mayan_date.long_count = [params[:lc][:kin].to_i, params[:lc][:winal].to_i, params[:lc][:tun].to_i, params[:lc][:katun].to_i, params[:lc][:baktun].to_i]
+            rescue
+                # In the case of an invalid Long Count URL, reset to the dawn of creation
+                @mayan_date.mayan_day = 0;
+            end
+
+        elsif params[:commit] == '+1' or params[:commit] == '-1' then
+
+            # Set the mayan date to the requested Mayan Day (hidden functionality for fixed-date buttons)
+            @mayan_date.mayan_day = params[:mdc].to_i
+
         else
+
             # Set the mayan date to the current date
             @mayan_date.gregorian = Date.today
+
         end
     end
+
+
 
     def about
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
     get '/home',        to: 'xmucane#home'
-    get '/home/(:date)',to: 'xmucane#home', constraints: {:date => /-?\d{4}-\d{2}-\d{2}/ }, as: 'home_date'
 
     get '/about',       to: 'xmucane#about'
 
