09b853bb8a0bd2a6d86be201f0366e9412bb1692
Thomas Pinkava <pinkavaprime@gmail.com>
2020-03-31 13:00:02 -0700

    HW8

--- app/models/factoid.rb
+++ app/models/factoid.rb
@@ -3,7 +3,9 @@ require 'mayan_date'
 class Factoid < ApplicationRecord
     # Valid Glyphs are the Tzolkin Veintenas, the Haab months, the Long Count Radices, and the Night Lords.
     @@ValidGlyphs = MayanDate.TZOLKIN_VEINTENAS + MayanDate.HAAB_MONTHS + ["baktun", "katun", "tun", "winal", "kin"] + (1..9).to_a.collect{|x| "G#{x}"}
-
+    def validGlyphs
+        @@ValidGlyphs
+    end
     validates(:content, presence: true)
     validates(:glyph_key, inclusion: { in: @@ValidGlyphs})
 end
